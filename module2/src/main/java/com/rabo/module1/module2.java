74f431a3f199c5e44cfbc796c6f7c28f2e8f99c3
RABODEVEU\pailas <sreenu.paila@gmail.com>
2018-10-15 11:32:51 +0200

    add

--- module2/src/main/java/com/rabo/module1/module2.java
+++ module2/src/main/java/com/rabo/module1/module2.java
@@ -18,6 +18,7 @@ public class module2 {
         System.out.println("Module 2");System.out.println("Module 2");
         System.out.println("Module 2");System.out.println("Module 2");
         System.out.println("Module 2");
+        System.out.println("Module 2");
 
     }
 }
